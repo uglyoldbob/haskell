1647de2c5fc46a40cb5c7e0f625ca22cbc39741b
Thomas Epperson <tepperson@briskheat.com>
2019-10-18 15:53:07 -0400

    Create function for generating probability density function. Label generated graphics.

--- gui/UglyStatistics.hs
+++ gui/UglyStatistics.hs
@@ -20,3 +20,20 @@ discreteCDF x n = zip bins scaledbinvals
        scaledbinvals = map (/ total) dbinvals
 
 lessOrEqualCount l n = length (filter (<= n) l)
+betweenValues l va vb = length l2
+    where l1 = filter (>= va) l
+          l2 = filter (<= vb) l1
+
+discretePDF x n = zip bins scaledbinvals
+ where dx = notEmpty x
+       dn = fromIntegral n
+       sorted = List.sort dx
+       minval = head sorted
+       maxval = last sorted
+       total = fromIntegral $ length dx
+       bins = [minval + (maxval-minval)*(fromIntegral t) / dn | t <- [1..n]]
+       binvals = forfun2 bins (betweenValues dx)
+       dbinvals = map fromIntegral binvals
+       scaledbinvals = map (/ total) dbinvals
+
+forfun2 x f = zipWith f ([0.0]++x) x
--- gui/reliability.hs
+++ gui/reliability.hs
@@ -30,8 +30,8 @@ main = do
 
         -- Retrieve some objects from the UI
         window <- G.builderGetObject builder G.castToWindow "window1"
-        button <- G.builderGetObject builder G.castToButton "button1"
         canvas <- G.builderGetObject builder G.castToDrawingArea "drawingarea1"
+        canvas2 <- G.builderGetObject builder G.castToDrawingArea "drawingarea2"
         text1 <- G.builderGetObject builder G.castToTextView "textview1"
         num_bins_spinner <- G.builderGetObject builder G.castToSpinButton "spinbutton1"
         
@@ -44,14 +44,22 @@ main = do
         G.on tb G.bufferChanged $ liftIO $ do
             things <- textViewString text1
             G.widgetQueueDraw canvas
-        G.afterValueSpinned num_bins_spinner (G.widgetQueueDraw canvas)
+            G.widgetQueueDraw canvas2
+        G.afterValueSpinned num_bins_spinner $ do
+            G.widgetQueueDraw canvas
+            G.widgetQueueDraw canvas2
         
         G.on canvas G.draw $ do
             dat <- liftIO $ textViewString text1
             numbins <- liftIO $ G.spinButtonGetValue num_bins_spinner
             let lst = csvToFloats dat
-            render3 canvas lst (maximum [5,round numbins])
+            render3 canvas2 lst (maximum [5,round numbins])
 
+        G.on canvas2 G.draw $ do
+            dat <- liftIO $ textViewString text1
+            numbins <- liftIO $ G.spinButtonGetValue num_bins_spinner
+            let lst = csvToFloats dat
+            render4 canvas2 lst (maximum [5,round numbins])
 
         -- Display the window
         G.widgetShowAll window
@@ -92,6 +100,28 @@ render3 canvas list numbins = do
     grid 0 (fromIntegral w) 0 (fromIntegral h)
     let calcm = (maximum list) * 1.1
     plotDots $ prepareDots 0 ((fromIntegral w)/calcm) 0 ((fromIntegral h)*0.9) $ discreteCDF list numbins
+    C.scale 1 (-1)
+    C.selectFontFace "monospace" C.FontSlantNormal C.FontWeightBold
+    C.setFontSize 12.0
+    C.moveTo 5.0 (-5.0)
+    C.showText "Cumulative Distribution Function"
+
+render4 :: G.DrawingArea -> [Double] -> Int -> C.Render()
+render4 canvas list numbins = do
+    C.setSourceRGB 1 1 1
+    C.paint
+    C.setSourceRGB 0 0 0
+    G.Rectangle _x _y w h <- liftIO (G.widgetGetClip canvas)
+    C.translate 0 $ (fromIntegral (h))
+    C.scale 1 (-1)
+    grid 0 (fromIntegral w) 0 (fromIntegral h)
+    let calcm = (maximum list) * 1.1
+    plotDots $ prepareDots 0 ((fromIntegral w)/calcm) 0 ((fromIntegral h)*0.9) $ discretePDF list numbins
+    C.scale 1 (-1)
+    C.selectFontFace "monospace" C.FontSlantNormal C.FontWeightBold
+    C.setFontSize 12.0
+    C.moveTo 5.0 (-5.0)
+    C.showText "Probability Density Function"
 
 prepareDot x1 x2 y1 y2 (x, y) = ((x+x1)*x2, (y+y1)*y2)
 
